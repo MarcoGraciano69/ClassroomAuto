5b21b9af8266a07950d40a04d1d76034800c4586
MarcoGraciano69 <88803532+MarcoGraciano69@users.noreply.github.com>
2026-03-15 22:19:07 -0600

    Update app.py

--- classroomAuto/app.py
+++ classroomAuto/app.py
@@ -29,7 +29,8 @@ def get_credentials():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            import os, json
+            flow = InstalledAppFlow.from_client_config(json.loads(os.environ["CREDENTIALS_JSON"]), SCOPES)
             creds = flow.run_local_server(port=0)
 
         with open("token.json", "w") as token_file:
